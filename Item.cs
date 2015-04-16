06578292823969fd1ced77b45bf8917d5f9b973a
Yury Rachkouski <6314166@gmail.com>
2015-04-16 16:28:53 +0300

    Task2
    Начальная разработка

---
+++ Item.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Item
+    {
+        private string[] _names = {"Книги",};
+         
+        private string _name;
+        private float _price;
+        private int _quantity;
+
+        public string Name
+        {
+            get { return this._name; } 
+            set { this._name = value; }
+        }
+        public float Price
+        {
+            get { return this._price; }
+            set { this._price = value; }
+        }
+        public int Quantity {
+            get {return this._quantity;}
+            set { this.Quantity = _quantity; }
+        }
+        public float WholeCost()
+        {
+            return this.Price*this.Quantity;
+        }
+    }
+}
